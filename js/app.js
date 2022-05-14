d23ef4080450391540f19f5e0fd0dbc390afc20c
Ricardo Macias <ricardomacias392@gmail.com>
2022-05-14 14:09:38 -0600

    El buscador esta activo y funcionando y muestra las estadisticas de la ciudad seleccionada, falta los mediaqueries, y hacer que cambie de fahrenheit a centigrados o viceversa. El proyecto sera pausado por un tiempo por limite alcanzado de peticiones.

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ const options = {
 
 // Variables
 const search_input = document.querySelector("#search_input");
+const search_button = document.querySelector("#search_button");
 const current_image_weather = document.querySelector("#current_image_weather");
 const temperature = document.querySelector("#temperature");
 const name_weather = document.querySelector("#name");
@@ -32,10 +33,20 @@ const visibilidad = document.querySelector("#visibility");
 const pressure = document.querySelector("#pressure");
 const cards = document.querySelector("#cards");
 const list_result = document.querySelector("#list_result");
-
+const search_box = document.querySelector("#search_box");
+const centigrades = document.querySelector("#centigrades").addEventListener("focus", console.log("Centigrados activados"))
+const fahrenheit = document.querySelector("#fahrenheit").addEventListener("focus", console.log("Fahrenheit activados"))
 // Event Listeners
 document.addEventListener("DOMContentLoaded", ObtenerLocacion());
-search_input.addEventListener("input", () => console.log(search_input.value))
+search_button.addEventListener("click", () => BuscarPais() )
+const search = document.querySelector("#search").addEventListener("click" , () => {
+    search_box.classList.remove("desactive")
+    search_box.classList.add("active");
+})
+const close_search = document.querySelector("#close_search").addEventListener("click", () => {
+    search_box.classList.remove("active")
+    search_box.classList.add("desactive");
+})
 
 
 
@@ -77,15 +88,22 @@ function ObtenerClimaActual(ciudad){
     .catch(err => console.error(err));
 }
 
-function BuscarPais(name){
-    fetch(`https://community-open-weather-map.p.rapidapi.com/find?q=${name}&cnt=2&type=like&units=metric`, options)
-	.then(response => response.json())
-	.then(response => {
-        console.log("Buscar Pais");
-        console.log(response)
-        ImprimirLista(response)
-    })
-	.catch(err => console.error(err));
+function BuscarPais(){
+    const busqueda = search_input.value;
+    if(busqueda === ""){
+        console.log("El campo esta vacio");
+        return;
+    }else{
+        fetch(`https://community-open-weather-map.p.rapidapi.com/find?q=${busqueda}&cnt=2&type=like&units=metric`, options)
+        .then(response => response.json())
+        .then(response => {
+            console.log("Buscar Pais");
+            console.log(response)
+            limpiarListaHTML();
+            ImprimirLista(response)
+        })
+        .catch(err => console.error(err));
+    }
 }
 
 function ImprimirLista(objeto){
@@ -96,6 +114,9 @@ function ImprimirLista(objeto){
         const paisHTML = document.createElement("li");
         paisHTML.textContent = nombre;
         paisHTML.classList.add("result");
+        paisHTML.dataset.lat = lat;
+        paisHTML.dataset.lon = lon;
+        paisHTML.onclick = () => ObtenerNombre(lat,lon)
         list_result.appendChild(paisHTML);
     })
 
@@ -104,6 +125,9 @@ function ImprimirLista(objeto){
 
 
 function ImprimirHTML5Dias(lista){
+    while(cards.firstChild){
+        cards.removeChild(cards.firstChild)
+    }
     let contador = 0;
     lista.forEach( dia => {
         contador++;
@@ -135,9 +159,14 @@ function ImprimirHTMLCurrent(objeto){
     const wind_degrees = wind.deg;
 
     // Imagen grande en el side
-    const image_icon = document.createElement("img")
-    image_icon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
-    current_image_weather.appendChild(image_icon);
+    // const image_icon = document.createElement("img")
+    // image_icon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+    // current_image_weather.appendChild(image_icon);
+
+    current_image_weather.innerHTML = `
+        <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="">
+    `
+
     // Temperatura
     temperature.innerHTML = `
         <p><b>${temperatura}</b>°C</p>
@@ -147,13 +176,29 @@ function ImprimirHTMLCurrent(objeto){
         <p>${description}</p>
     `
     // Fecha de hoy
-    const fechaHTML = document.createElement("p")
-    fechaHTML.textContent = moment().format("MMMM Do YYYY");
-    date.appendChild(fechaHTML);
+    // const fechaHTML = document.createElement("p")
+    // fechaHTML.textContent = moment().format("MMMM Do YYYY");
+    // date.appendChild(fechaHTML);
+
+    date.innerHTML = `
+        <p>Today</p>
+        <p> ${moment().format("MMMM Do YYYY")} </p>
+    `
+
     // Nombre de la ciudad
-    const nombreCiudad = document.createElement("p");
-    nombreCiudad.textContent = name;
-    city_name.appendChild(nombreCiudad);
+    // const nombreCiudad = document.createElement("p");
+    // nombreCiudad.textContent = name;
+    // city_name.appendChild(nombreCiudad);
+
+    city_name.innerHTML = `
+    <div class="icon-location">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+        </svg>
+    </div>
+        <p> ${name} </p>
+    `
+
     // Estado del viento (Wind Status)
     wind_status.innerHTML = `
     <p>Wind status</p>
@@ -189,6 +234,12 @@ function ImprimirHTMLCurrent(objeto){
         <p>Air Pressure</p>
         <p class="number"><b>${pressAire}</b> mb</p>
     `
+}
+
+function limpiarListaHTML(){
+    while(list_result.firstChild){
+        list_result.removeChild(list_result.firstChild);
+    }
 }
 
 
